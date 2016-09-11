875917103f554a38d7fa94e0c60959d501a77846
AO Industries, Inc. <support@aoindustries.com>
2016-09-11 00:59:55 -0500

    Added RSS feed.

---
+++ src/main/java/com/semanticcms/news/style/NewsStyleContextListener.java
@@ -0,0 +1,45 @@
+/*
+ * semanticcms-news-style - Default style for SemanticCMS newsfeeds.
+ * Copyright (C) 2016  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-news-style.
+ *
+ * semanticcms-news-style is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-news-style is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-news-style.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.news.style;
+
+import com.semanticcms.core.servlet.SemanticCMS;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener("Registers the styles for SemanticCMS newsfeeds.")
+public class NewsStyleContextListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		// TODO: Only add this style to the news view
+		SemanticCMS semanticCMS = SemanticCMS.getInstance(event.getServletContext());
+		// Add our CSS file
+		semanticCMS.addCssLink("/semanticcms-news-style/styles.css");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		// Do nothing
+	}
+}
