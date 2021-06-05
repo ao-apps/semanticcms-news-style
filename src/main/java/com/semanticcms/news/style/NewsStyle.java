3ff5600170f7d6f4c66abf187ce19484e72c3e38
AO Industries, Inc <support@aoindustries.com>
2021-06-05 01:49:59 -0500

    Open-source projects moved to "com.aoapps" packages

--- src/main/java/com/semanticcms/news/style/NewsStyle.java
+++ src/main/java/com/semanticcms/news/style/NewsStyle.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-news-style - Default style for SemanticCMS newsfeeds.
- * Copyright (C) 2016, 2017, 2020  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2020, 2021  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,9 +22,9 @@
  */
 package com.semanticcms.news.style;
 
-import com.aoindustries.web.resources.registry.Group;
-import com.aoindustries.web.resources.registry.Style;
-import com.aoindustries.web.resources.servlet.RegistryEE;
+import com.aoapps.web.resources.registry.Group;
+import com.aoapps.web.resources.registry.Style;
+import com.aoapps.web.resources.servlet.RegistryEE;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
