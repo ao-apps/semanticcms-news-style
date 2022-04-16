2ac9138f06d2222f6c555026216aeee9cbe12136
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:02:41 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- book/src/main/java/com/semanticcms/news/style/book/ModularApidocsRedirect.java
+++ book/src/main/java/com/semanticcms/news/style/book/ModularApidocsRedirect.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-news-style - Default style for SemanticCMS newsfeeds.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-news-style.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.news.style.book;
 
 import com.aoapps.net.URIParametersUtils;
